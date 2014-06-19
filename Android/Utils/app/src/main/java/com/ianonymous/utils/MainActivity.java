e66420ba2b03c6060c28517cf243a2f51621ac9f
txlong <txlong_onz@163.com>
2014-06-19 15:27:41 +0800

    版本工具类完善
    添加获得版本信息的工具类

--- Android/Utils/app/src/main/java/com/ianonymous/utils/MainActivity.java
+++ Android/Utils/app/src/main/java/com/ianonymous/utils/MainActivity.java
@@ -2,13 +2,22 @@ package com.ianonymous.utils;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ianonymous.utils.tools.VersionUtils;
 
 
 public class MainActivity extends Activity {
+    private TextView mVersionTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVersionTv = (TextView) findViewById(R.id.versionTv);
+        mVersionTv.setText("当前应用的版本名称是：" + VersionUtils.getVersionName(this));
+        mVersionTv.append("\n当前应用的版本号是：" + VersionUtils.getVersionCode(this));
+        mVersionTv.append("\n当前设备的Android版本为：" + VersionUtils.getDeviceVersion());
     }
 }
--- Android/Utils/app/src/main/java/com/ianonymous/utils/tools/VersionUtils.java
+++ Android/Utils/app/src/main/java/com/ianonymous/utils/tools/VersionUtils.java
@@ -3,22 +3,58 @@ package com.ianonymous.utils.tools;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
 /**
- * Created by Administrator on 2014/6/19.
+ * Created by Leo on 2014/6/19.
  */
 public class VersionUtils {
+    /**
+     * 根据应用上下文获得当前应用的Version Name
+     *
+     * @param context 应用上下文
+     * @return 当前应用的版本名称
+     */
     public static String getVersionName(Context context) {
-        String version = "NAN";
+        String versionName = "NAN";
         PackageManager packageManager = context.getPackageManager();
         PackageInfo packInfo = null;
         try {
             // getPackageName()是你当前类的包名，0代表是获取版本信息
             packInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
-            version = packInfo.versionName;
+            versionName = packInfo.versionName;
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+        } finally {
+            return versionName;
         }
-        return version;
+    }
+
+    /**
+     * 根据应用上下文获得当前应用的Version Code
+     *
+     * @param context 应用上下文
+     * @return 当前应用的版本号
+     */
+    public static int getVersionCode(Context context) {
+        int versionCode = 0;
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packInfo = null;
+        try {
+            // getPackageName()是你当前类的包名，0代表是获取版本信息
+            packInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = packInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            return versionCode;
+        }
+    }
+
+    /**
+     * @return 当前手机的Android版本
+     */
+    public static String getDeviceVersion() {
+        return Build.VERSION.RELEASE;
     }
 }
