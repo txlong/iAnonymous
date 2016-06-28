8e807ffb66f9a517b6efb678b525dea849c84a37
唐晓龙 <txlong_onz@163.com>
2016-06-28 19:21:30 +0800

    添加BottomNavigationBar

--- Android/MaterialTxl/app/src/main/java/com/ianonymous/materialtxl/MainActivity.java
+++ Android/MaterialTxl/app/src/main/java/com/ianonymous/materialtxl/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ianonymous.materialtxl;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.IdRes;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,7 +14,12 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarBadge;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 findViewById(R.id.editText1).setEnabled(b);
             }
         });
+
+//        mBottomBar = BottomBar.attach(this, savedInstanceState);
+
+        // Instead of attach(), use attachShy():
+        mBottomBar = BottomBar.attachShy((CoordinatorLayout) findViewById(R.id.myCoordinator),
+                findViewById(R.id.fragment), savedInstanceState);
+
+        mBottomBar.setItems(R.menu.bottombar_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    // The user selected item number one.
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    // The user reselected item number one, scroll your content to top.
+                }
+            }
+        });
+
+        // Setting colors for different tabs when there's more than three of them.
+        // You can set colors for tabs in three different ways as shown below.
+        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+        mBottomBar.mapColorForTab(1, 0xFF5D4037);
+        mBottomBar.mapColorForTab(2, "#7B1FA2");
+        mBottomBar.mapColorForTab(3, "#FF5252");
+        mBottomBar.mapColorForTab(4, "#FF9800");
+
+        // Make a Badge for the first tab, with red background color and a value of "13".
+        BottomBarBadge unreadMessages = mBottomBar.makeBadgeForTabAt(1, "#FF0000", 13);
+
+        // Control the badge's visibility
+        unreadMessages.show();
+
+        // Change the displayed count for this badge.
+        unreadMessages.setCount(4);
+
+        // Change the show / hide animation duration.
+        unreadMessages.setAnimationDuration(200);
+
+        // If you want the badge be shown always after unselecting the tab that contains it.
+        unreadMessages.setAutoShowAfterUnSelection(true);
+
+        // If you don't want this badge to be hidden after selecting the tab contains it.
+//        unreadMessages.setAutoShowAfterUnSelection(false);
     }
 
     @Override
@@ -67,5 +123,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
     }
 }
